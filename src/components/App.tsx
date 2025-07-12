0a587143942e97ac49a1e7169ef57032179365c6
Olexandr <sashokseven@gmail.com>
2025-07-12 15:46:04 +0300

    create comp SearchBar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import css from "./App.module.css";
+import SearchBar from "./SearchBar/SearchBar";
+import { fetchMovies } from "../services/movieService";
+import type { Movie } from "../types/movie";
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const handleSearch = async (movie: string) => {
+    try {
+      const data = await fetchMovies(movie);
+      if(data.length === 0){
+        toast.error('No movies found for your request.')
+      }
+      setMovies(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={css.app}>
-      <p>hello </p>
+      <SearchBar onSubmit={handleSearch} />
+      <Toaster />
     </div>
   );
 }
